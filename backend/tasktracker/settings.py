7a2adcdf390a9c372913699943a6bd8dfb771e4a
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2021-06-13 15:36:15 +0300

    Add cors headers

--- backend/tasktracker/settings.py
+++ backend/tasktracker/settings.py
@@ -27,12 +27,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'users.apps.UsersConfig',
     'tasks.apps.TasksConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
